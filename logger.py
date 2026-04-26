47d99fb958b1207e1eeb9bff41fc38199604ea70
dan.code <jomahakumoza57280@gmail.com>
2026-04-27 00:16:51 +0200

    add add type annotations and docstrings

--- logger.py
+++ logger.py
@@ -1,38 +1,52 @@
 import logging
-from logging.handlers import RotatingFileHandler
 
 
-def setup_logger(log_file='app.log', max_bytes=5*1024*1024, backup_count=5):
+def setup_logger(name: str, log_file: str, level: int = logging.INFO) -> logging.Logger:
     """
-    Setup a rotating logger.
+    Setup a logger with the specified name and log file.
 
-    :param log_file: Name of the log file.
-    :param max_bytes: Maximum file size before rotation.
-    :param backup_count: Number of backup files to keep.
+    Args:
+        name (str): The name of the logger.
+        log_file (str): The log file path to write logs.
+        level (int): The logging level (default is INFO).
+
+    Returns:
+        logging.Logger: Configured logger instance.
     """
-    # Create a logger object
-    logger = logging.getLogger('my_logger')
-    logger.setLevel(logging.DEBUG)
+    # Create a logger
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
 
-    # Create a rotating file handler
-    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
-    handler.setLevel(logging.DEBUG)
+    # Create file handler that logs to a file
+    fh = logging.FileHandler(log_file)
+    fh.setLevel(level)
 
-    # Create a formatter and set it for the handler
+    # Create formatter and add it to the handler
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
+    fh.setFormatter(formatter)
 
-    # Add the handler to the logger
-    logger.addHandler(handler)
-
+    # Add the file handler to the logger
+    logger.addHandler(fh)
     return logger
 
 
-# Example usage
-if __name__ == '__main__':
-    log = setup_logger()
-    log.debug('Debug message')
-    log.info('Info message')
-    log.warning('Warning message')
-    log.error('Error message')
-    log.critical('Critical message')
+def log_info(logger: logging.Logger, message: str) -> None:
+    """
+    Log an information message.
+
+    Args:
+        logger (logging.Logger): The logger instance to use.
+        message (str): The information message to log.
+    """
+    logger.info(message)
+
+
+def log_error(logger: logging.Logger, message: str) -> None:
+    """
+    Log an error message.
+
+    Args:
+        logger (logging.Logger): The logger instance to use.
+        message (str): The error message to log.
+    """
+    logger.error(message)
